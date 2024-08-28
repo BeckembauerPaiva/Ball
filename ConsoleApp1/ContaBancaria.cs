50664a231fe6f510addbe0c1f0af60ef6070fa6b
BeckembauerPaiva <168041127+BeckembauerPaiva@users.noreply.github.com>
2024-08-27 21:24:40 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp1/ContaBancaria.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace ConsoleApp1
+{
+    internal class ContaBancaria
+    {
+        public int NumeroDaConta {  get;private set; }
+        public string TitularDaConta { get; set; }
+        public double SaldoDaConta { get;private set; }
+
+        public ContaBancaria(int numeroDaConta, string titularDaConta)
+        {
+            NumeroDaConta = numeroDaConta;
+            TitularDaConta = titularDaConta;
+        }
+        public double Deposito(double valor)
+        {
+            return SaldoDaConta += valor;
+        }
+        public double Saque(double valor)
+        {
+            return SaldoDaConta -= valor-5.00;
+        }
+       
+
+        public ContaBancaria(int numeroDaConta, string titularDaConta, double saldoDaConta) : this(numeroDaConta, titularDaConta)
+        {
+            SaldoDaConta = saldoDaConta;
+        }
+        public override string ToString()
+        {
+            return "Conta "
+                + NumeroDaConta
+                + ", Titular: "
+                + TitularDaConta
+                + ", Saldo: $ "
+                + SaldoDaConta.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,72 @@
+﻿
+using ConsoleApp1;
+using System;
+using System.Globalization;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaBancaria Conta;
+            Console.Write("Entre com o número da conta: ");
+            int Num = int.Parse(Console.ReadLine());
+            Console.Write( "Entre o titular da conta: " );
+            string Nome=Console.ReadLine();
+            Console.Write("Hávera depósito inicial (s/n):");
+           
+   
+            char Pergunta = char.Parse(Console.ReadLine());
+            if (Pergunta == 's'||Pergunta=='S') 
+            {
+                Console.Write("Entre o valor do depósito inicial: ");
+                double DepositoInicial=double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+                Conta = new ContaBancaria(Num,Nome,DepositoInicial);
+                Console.WriteLine("Dados da conta:");
+                Console.WriteLine(Conta);
+                Console.WriteLine();
+                Console.Write("Entre o valor para depósito: ");
+                double deposito = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Conta.Deposito(deposito);
+                Console.WriteLine(Conta);
+                Console.WriteLine();
+                Console.Write("Entre com o valor para saque: ");
+                double saque = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Conta.Saque(saque);
+                Console.WriteLine(Conta);
+
+            }
+            else if (Pergunta=='n'||Pergunta=='N')
+
+            {
+                double DepositoInicial= 0;
+                Conta = new ContaBancaria(Num, Nome, DepositoInicial);
+                Console.WriteLine("Dados da conta:");
+                Console.WriteLine(Conta);
+                Console.WriteLine();
+                Console.Write("Entre o valor para depósito: ");
+                double deposito = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Conta.Deposito(deposito);
+                Console.WriteLine(Conta);
+                Console.WriteLine();
+                Console.Write("Entre com o valor para saque: ");
+                double saque = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Conta.Saque(saque);
+                Console.WriteLine(Conta);
+
+
+
+            }
+            
+
+
+
+
+
+
+
+
+        }
+    }
+}
